da7094ff12d9a8d877eca3eee08bf9ad6321b394
neutralradiance <williamluke@me.com>
2020-10-28 21:04:51 -0500

    Initial push

---
+++ Sources/Unwrap/Infallible.swift
@@ -0,0 +1,12 @@
+//
+//  File.swift
+//  
+//
+//  Created by neutralradiance on 10/22/20.
+//
+
+import Foundation
+
+public protocol Infallible {
+    static var defaultValue: Self { get }
+}
---
+++ Sources/Unwrap/Unrap+Optional.swift
@@ -0,0 +1,45 @@
+//
+//  File.swift
+//  
+//
+//  Created by neutralradiance on 10/20/20.
+//
+
+import Foundation
+
+extension Optional where Wrapped: Infallible {
+    public var unwrap: Wrapped { self ?? Wrapped.defaultValue }
+}
+
+extension Bool: Infallible {
+    public static var defaultValue: Self { true }
+}
+
+extension String: Infallible {
+    public static var defaultValue: Self { "" }
+}
+
+extension Int: Infallible {
+    public static var defaultValue: Self { 0 }
+}
+
+extension Int64: Infallible {
+    public static var defaultValue: Self { 0 }
+}
+
+extension Array: Infallible where Element: RandomAccessCollection {
+    public static var defaultValue: Self { [] }
+}
+
+extension UUID: Infallible {
+    public static var defaultValue: Self { UUID() }
+}
+
+extension Date: Infallible {
+    public static var defaultValue: Self { Date() }
+}
+
+extension Data: Infallible {
+    public static var defaultValue: Self { Data() }
+}
+
